0f299b872ea4a6663ae7e6459dee2a597b0a0b35
pogersxd <diegoraguimaraes@gmail.com>
2025-10-10 16:03:46 -0300

    Update header.php

--- components/header.php
+++ components/header.php
@@ -6,7 +6,7 @@
     return <<<HTML
         <div class="header">
           <div class="header__icon-search-bar">
-            <a class="header__icon" href="./"><img class="header__icon" src="assets/james.png" alt="Foto da James Sunderland"></a>
+            <a class="header__icon" href="./profile.php"><img class="header__icon" src="assets/james.png" alt="Foto da James Sunderland"></a>
             $html
           </div>     
           <div class="header__link">
