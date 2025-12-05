f0db26152f519f52ec6781184d7e9a60d5eb5746
pogersxd <diegoraguimaraes@gmail.com>
2025-12-04 22:11:51 -0300

    alterei um pequeno erro no icone de play

--- components/singleItemArtista.php
+++ components/singleItemArtista.php
@@ -1,16 +1,15 @@
  <?php
-    function renderSingleItemArtista($id_usuario, $nome, $foto)
-    {
+  function renderSingleItemArtista($id_usuario, $nome, $foto)
+  {
 
-        return <<<HTML
+    return <<<HTML
               <div class="single-item single-item__artist">
-                <a href="#" onclick="loadProfile('{$id_usuario}')" class="single-item__image-button">
-                  <img class="single-item__image single-item__image-artist" src="./assets/pfps/{$foto}" alt="Foto do artista {$nome}">
-                  <a href="#" class="fa-solid fa-circle-play single-item__icon"></a>
-                </a>
+                <div class="single-item__image-button">
+                  <a onclick="loadProfile('{$id_usuario}')" href="#"><img class="single-item__image single-item__image-artist" src="./assets/pfps/{$foto}" alt="Foto do artista {$nome}"></a>
+                </div>
                 <div class="single-item__texts">
-                  <a href="#" class="single-item__texts-title">{$nome}</a><br>
+                  <a href="#" onclick="loadProfile('{$id_usuario}')" class="single-item__texts-title">{$nome}</a><br>
                 </div>
               </div>
             HTML;
-    }
+  }
