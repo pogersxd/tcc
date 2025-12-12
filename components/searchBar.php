206a16093fe6dd855b44e1cd998586f4e3d771e0
pogersxd <diegoraguimaraes@gmail.com>
2025-12-12 16:37:30 -0300

    Update searchBar.php

--- components/searchBar.php
+++ components/searchBar.php
@@ -2,13 +2,16 @@
     <?php
     function renderSearchBar()
     {
-      return <<<HTML
+        return <<<HTML
     <form class="header__search-bar" onsubmit="return false;">
-        <input 
-            type="text"
-            placeholder="Buscar músicas, álbuns ou artistas..."
-            oninput="search(this.value)"
-        >
+        <label>
+            <i class="fa-solid fa-magnifying-glass"></i>
+            <input 
+                type="text"
+                placeholder="Buscar músicas, álbuns ou artistas..."
+                oninput="search(this.value)"
+            >
+        </label>
     </form>
     HTML;
     }
